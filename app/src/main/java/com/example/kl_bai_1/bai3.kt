e6c82aa29a040ae57b2caee0eb368940c3518460
luc tien <lucd7464@gmail.com>
2024-05-14 08:51:47 +0700

    Thông điệp commit của bạn

---
+++ app/src/main/java/com/example/kl_bai_1/bai3.kt
@@ -0,0 +1,27 @@
+//package com.example.kl_bai_1
+//
+//fun main() {
+//val a=5
+//    val nhanDoi2=a.let{
+//        it *2
+//    }
+//
+//    val tenInHoa= with(tenSV){}
+//
+//}
+//
+//val tinhTong={soA:Int,soB:Int -> {
+//    val  a=10
+//    val  b=10
+//    val  c=10
+//    (soA+soB)}
+//}
+//val  chiaDoi={soA:Float ->soA/2}
+//fun getStringLength(str: String?):Int?{
+//    return  str?.run {
+//        if(isEmpty())
+//            return 0
+//        length
+//    }
+//}
+//
--- app/src/main/java/com/example/kl_bai_1/lab1.kt
+++ app/src/main/java/com/example/kl_bai_1/lab1.kt
@@ -1,104 +1,70 @@
 package com.example.kl_bai_1
 
 fun main() {
-    println("Nguyễn Văn Anh - PS123456 ")
-    println("Toi yeu viet nam ")
-    println("Toi yeu viet nam ")
-    println("Toi yeu viet nam ")
+    bai1()
+    bai2()
+    bai3()
+
+}
+
+fun bai1() {
     var a = 0.0
     var b = 0.0
-    println("Nhập a")
+    println("Nhập a:")
     var s = readLine()
-    if (s != null) a = s.toDouble();
-    println("Nhập b")
+    if (s != null) a = s.toDouble()
+    println("Nhập b:")
     s = readLine()
-    if (s != null) b = s.toDouble();
-    println("Tổng:" + (a + b))
-    println("Hiệu:" + (a - b))
-    println("Tích:" + (a * b))
-    println("Thương:" + (a / b))
-
-    //vd1
-    println("Hello, World!")
-    //vd2
-    printMessage("Hello")
-    // vd3
-    println(sum(1, 2))
-    // vd4
-    println(multiply(2, 4))
-    // vd5
-    println(3 add 3)
-    // vd6
-    println("v" equar "c")
-    // vd7
-    val point1 = Point(1, 2)
-    val point2 = Point(3, 4)
-    val sum = point1 + point2
-    println("Sum: (${sum.x}, ${sum.y})") // Kết quả sẽ là Sum: (4, 6)
-    //vd8
-    printNumbers(1, 2, 3, 4, 5)
-    //vd9
-    var neverNull: String = "This can't be null"            // 1
-
-    // 2
-
-    var nullable: String? = "You can keep a null here"      // 3
-
-
-    println(strLength(neverNull)) // In ra độ dài của chuỗi neverNull
-    println(
-        strLength(
-            nullable ?: ""
-        )
-    ) // In ra độ dài của chuỗi nullable, xử lý null-safe với elvis operator
-    //vd10
-
-    val customer = Customer()                   // 3
-
-    val contact = Contact(1, "mary@gmail.com")  // 4
-
-    println(contact.id)                         // 5
-    contact.email = "jane@gmail.com"            // 6
-
-}
-
-fun printMessage(message: String) {                               // 1
-    println(message)
-}
-
-fun sum(x: Int, y: Int): Int {                                          // 3
-    return x + y
-}
-
-fun multiply(x: Int, y: Int) = x * y
-
-infix fun Int.add(b: Int): Int = this + b;
-infix fun String.equar(b: String): Boolean = this == b;
-
-class Point(val x: Int, val y: Int) {
-    operator fun plus(other: Point): Point {
-        return Point(x + other.x, y + other.y)
+    if (s != null) b = s.toDouble()
+    if (a == 0.0 && b == 0.0) {
+        println("Phương trình vô số nghiệm")
+    } else if (a == 0.0 && b != 0.0) {
+        println("Phương trình vô nghiệm")
+    } else {
+        val x = -b / a
+        println("No x=" + x)
     }
 }
 
-class Point2(val x: Int, val y: Int) {
-    operator fun plus(other: Point): Point {
-        return Point(x + other.x, y + other.y)
+fun bai2() {
+    var month = 0
+    println("Nhập tháng:")
+    val s: String? = readLine()
+    if (s != null) month = s.toInt()
+    when (month) {
+        1, 2, 3 -> println("Tháng " + month + " thuộc quý 1")
+        4, 5, 6 -> println("Tháng " + month + " thuộc quý 2")
+        7, 8, 9 -> println("Tháng " + month + " thuộc quý 3")
+        10, 11, 12 -> println("Tháng " + month + " thuộc quý 4")
+        else -> println("Tháng " + month + " không hợp lệ")
     }
 }
 
-fun printNumbers(vararg numbers: Int) {
-    for (number in numbers) {
-        println(number)
-    }
+fun bai3() {
+    var year = 0
+    var s: String?
+    println("Chương trình kiểm tra năm nhuần:")
+    do {
+        println("Nhập 1 năm:")
+        s = readLine()
+        while (s == null || s.toInt() < 0) {
+            println("Nhập sai năm, nhập lại:")
+            s = readLine()
+        }
+        year = s.toInt()
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            println("Năm $year là năm nhuần")
+        } else {
+            println("Năm $year không phải là năm nhuần")
+        }
+        print("Tiếp không?(c/k):")
+        s = readLine()
+        if (s == "k")
+            break;
+    } while (true)
+    println("Kết thúc chương trình")
 }
 
-
-fun strLength(notNull: String): Int {                   // 7
-    return notNull.length
-}
-
-class Customer                                  // 1
 
 class Contact(val id: Int, var email: String)
 
